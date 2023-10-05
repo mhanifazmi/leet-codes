8abc097eb15be56e2224aa2a022e34458006ecd3
mhanifazmi <68853427+mhanifazmi@users.noreply.github.com>
2023-10-05 11:30:45 +0800

    Time: 82 ms (56.6%), Space: 25.8 MB (9.43%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.php
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.php
@@ -7,9 +7,11 @@ class Solution {
     function winnerOfGame($colors) {
         $alice_wins = $bob_wins = 0;
 
-        for ($i = 1; $i < strlen($colors) - 1; $i++) {
-            if ($colors[$i - 1] == $colors[$i] && $colors[$i] == $colors[$i + 1]) {
-                if ($colors[$i] == 'A') {
+        $array = str_split($colors);
+
+        foreach($array as $i => $arr){
+            if ($array[$i - 1] == $arr && $arr == $array[$i + 1]) {
+                if ($arr == 'A') {
                     $alice_wins++;
                 } else {
                     $bob_wins++;
