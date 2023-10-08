56ba5d93e3b799eda546703834dbb9a4f710c60f
mhanifazmi <68853427+mhanifazmi@users.noreply.github.com>
2023-10-08 10:14:29 +0800

    Time: 19 ms (36.48%), Space: 18.9 MB (87.8%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.php
@@ -0,0 +1,46 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function romanToInt($s) {
+        $test = [];
+        $number = $value = 0;
+        $skip = 0;
+        $symbols = [
+            'I',
+            'V',
+            'X',
+            'L',
+            'C',
+            'D',
+            'M',
+        ];
+
+        $values = [1, 5, 10, 50, 100, 500, 1000];
+
+        $reverse = strrev($s);
+
+        foreach(str_split($reverse) as $key => $rev){
+            if($skip == 1) {
+                $skip = 0;
+                continue;
+            }
+            $diff = array_search($rev, $symbols) - array_search($reverse[$key + 1], $symbols);
+
+            if(isset($reverse[$key + 1]) && $diff > 0 && $diff <= 2){
+                $value = abs($values[array_search($reverse[$key + 1], $symbols)] - $values[array_search($rev, $symbols)]);
+                $skip = 1;
+            }
+            else{
+                $value = $values[array_search($rev, $symbols)];
+            }
+            $number += $value;
+            $test[] = $value;
+        }
+        
+        // return print_r($test);
+        return $number;
+    }
+}
